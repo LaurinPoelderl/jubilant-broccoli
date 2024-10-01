5a393f901a03a8434ffc93d921077958d614c5ba
Michi4 <michael.ruep@gmail.com>
2024-10-01 15:52:27 +0200

    add: i bin nur driver

--- src/components/table/table-component.ts
+++ src/components/table/table-component.ts
@@ -1,5 +1,36 @@
 import { html, render } from "lit-html";
 
 class TableComponent extends HTMLElement {
-  connectedCallback() {}
+  static observedAttributes = []
+
+  constructor(){
+    super()
+  }
+
+  connectedCallback() {
+    this.render()
+  }
+
+  attributeChangedCallback(name, oldValue, newValue){
+    console.log("")
+  }
+
+  render(){
+    return html`
+      <table>
+        <thead>
+          <tr>
+            <th>
+              ID
+            </th>
+            <th>
+              NAME
+            </th>
+          </tr>
+        </thead>
+      </table>
+    `
+  }
 }
+
+customElements.define("table-component", TableComponent)
